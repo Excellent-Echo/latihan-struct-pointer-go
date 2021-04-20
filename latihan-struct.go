4eda62b7ab39e1f7f27d4acace1681b519f40bbf
unknown <panduwilanthara@gmail.com>
2021-04-20 16:19:15 +0700

    latihan-struct selesai yei

--- latihan-struct.go
+++ latihan-struct.go
@@ -13,23 +13,20 @@ type Statistic struct {
 	max int
 }
 
-// func createStruct()  {
-
-// }
 func main() {
 	v1 := Variadic{}
 	v1.score = append(v1.score, 76, 80, 50, 50, 60, 70)
 
-	// v1.statistic.max = v1.score[0]
-	// v1.statistic.min = v1.score[0]
+	v1.statistic.max = v1.score[0]
+	v1.statistic.min = v1.score[0]
 
 	for i := 0; i < len(v1.score); i++ {
 		v1.statistic.avg += v1.score[i] / 5
-		// if v1.score[i] > max {
-		// 	v1.statistic.max = v1.score[i]
-		// } else if v1.score[i] < min {
-		// 	v1.statistic.min = v1.score[i]
-		// }
+		if v1.score[i] > v1.statistic.max {
+			v1.statistic.max = v1.score[i]
+		} else if v1.score[i] < v1.statistic.min {
+			v1.statistic.min = v1.score[i]
+		}
 		if v1.statistic.avg > 50 {
 			v1.status = true
 		} else if v1.statistic.avg <= 50 {
@@ -37,5 +34,5 @@ func main() {
 		}
 	}
 
-	fmt.Println(v1.status, v1.score, v1.statistic.avg)
+	fmt.Printf("Status : %v,\nScore : %v,\nStatistic : {\naverage : %v\nminimum : %v\nmaximum : %v\n}", v1.status, v1.score, v1.statistic.avg, v1.statistic.min, v1.statistic.max)
 }
