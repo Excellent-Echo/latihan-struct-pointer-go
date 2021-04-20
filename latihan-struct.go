65e289e474b666e01df2a911757b63c5bbc43501
unknown <panduwilanthara@gmail.com>
2021-04-20 16:13:21 +0700

    latihan-struct sudah hampir selesai proses mencari min max

--- latihan-struct.go
+++ latihan-struct.go
@@ -20,11 +20,22 @@ func main() {
 	v1 := Variadic{}
 	v1.score = append(v1.score, 76, 80, 50, 50, 60, 70)
 
-	s1 := Statistic{}
+	// v1.statistic.max = v1.score[0]
+	// v1.statistic.min = v1.score[0]
 
 	for i := 0; i < len(v1.score); i++ {
-		s1.avg += v1.score[i] / 5
+		v1.statistic.avg += v1.score[i] / 5
+		// if v1.score[i] > max {
+		// 	v1.statistic.max = v1.score[i]
+		// } else if v1.score[i] < min {
+		// 	v1.statistic.min = v1.score[i]
+		// }
+		if v1.statistic.avg > 50 {
+			v1.status = true
+		} else if v1.statistic.avg <= 50 {
+			v1.status = false
+		}
 	}
 
-	fmt.Println(s1.avg)
+	fmt.Println(v1.status, v1.score, v1.statistic.avg)
 }
