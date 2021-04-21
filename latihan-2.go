31a8922cf182bb45de7d5e1db575ea28e2468725
mohammedazez <muhamadaziz047@gmail.com>
2021-04-21 22:34:52 +0700

    latihan 2

--- latihan-2.go
+++ latihan-2.go
@@ -2,30 +2,35 @@ package main
 
 import "fmt"
 
-func GetAverage(data []int, avg... int) {
+func GetAverage(data *[]int, avg... int) int{
+	var result int
 
 	lengthavg := len(avg)
-	lengthdata := len(data)
-	finalLength := lengthavg + lengthdata
-
 	newAvg := 0
 	for _, value := range avg {
 		newAvg += value
 	}
 
+
+	lengthdata := len(*data)
 	newData := 0
-	for _, value := range data {
+	for _, value := range *data {
 		newData += value
 	}
+	finalLength := lengthavg + lengthdata
 
-	result := (newAvg + newData) / finalLength
-	fmt.Println(result)
 
+	result = (newAvg + newData) / finalLength
+	return result
 }
 
 func main() {
-	var number = []int{80,80,90,90}
-	GetAverage(number, 70,70)
-	GetAverage(number, 60,60)
-	GetAverage(number, 50,40,80)
+	number := []int{80,80,90,90}
+	avg := GetAverage(&number , 70,70)
+	//avg2 := GetAverage(&number, 60,60)
+	//avg3 := GetAverage(&number, 50,40,80)
+
+	fmt.Println(avg)
+	//fmt.Println(avg2)
+	//fmt.Println(avg3)
 }
