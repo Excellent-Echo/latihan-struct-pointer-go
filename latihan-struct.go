6d9ea237d8f08ec2174fa8a4f9a009cdbb42eef0
unknown <panduwilanthara@gmail.com>
2021-04-21 15:02:24 +0700

    latihan-struct merapihkan code saja

--- latihan-struct.go
+++ latihan-struct.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 type Variadic struct {
-	status    bool  // false
-	score     []int // slice
+	status    bool
+	score     []int
 	statistic Statistic
 }
 type Statistic struct {
@@ -24,20 +24,23 @@ func GetStatistic(num ...int) {
 
 	for i := 0; i < len(v1.score); i++ {
 
-		total += v1.score[i] // rata rata
+		total += v1.score[i] // semua data dijumlah untuk dirata-rata
 
+		//max min
 		if v1.score[i] > v1.statistic.max {
 			v1.statistic.max = v1.score[i]
 		} else if v1.score[i] < v1.statistic.min {
 			v1.statistic.min = v1.score[i]
 		}
 
+		//status
 		if v1.statistic.avg > 50 {
 			v1.status = true
 		} else if v1.statistic.avg <= 50 {
 			v1.status = false
 		}
 
+		//rata rata final
 		v1.statistic.avg = total / len(v1.score)
 	}
 	fmt.Printf("Status : %v,\nScore : %v,\nStatistic : {\naverage : %v\nminimum : %v\nmaximum : %v\n}", v1.status, v1.score, v1.statistic.avg, v1.statistic.min, v1.statistic.max)
