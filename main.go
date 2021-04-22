8cb0a0610a3a71cb4750fdfb1cf37303bc9c28e2
ardhisr <radika.kerenz1@gmail.com>
2021-04-23 05:04:46 +0700

    done task 2

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+import "fmt"
+
+//Task 1
 type Statistic struct {
 	average int
 	minimum int
@@ -12,7 +15,7 @@ type Respon struct {
 	Statistic
 }
 
-func GetStatistic(numbers ...int) map[string]Respon {
+func GetStatistic(numbers ...int) Respon {
 	var (
 		sum, max, min, avg int
 	)
@@ -35,13 +38,36 @@ func GetStatistic(numbers ...int) map[string]Respon {
 	res.maximum = max
 	res.minimum = min
 
-	result := map[string]Respon{
-		"response": res,
-	}
-
-	return result
+	return res
 }
 
 func main() {
-	GetStatistic(76, 80, 50, 50, 60, 70)
+	// Task 1
+	stat := GetStatistic(76, 80, 50, 50, 60, 70)
+
+	//Task 2
+	var number = []int{80, 80, 90, 90}
+
+	avg := GetAverage(&number, 70, 70)
+
+	fmt.Println(stat, avg)
+}
+
+//Task 2
+func GetAverage(oldNums *[]int, newNums ...int) int {
+	tmp := make([]int, 0)
+	var sum int
+	for _, num := range *oldNums {
+		tmp = append(tmp, num)
+	}
+	for _, num := range newNums {
+		tmp = append(tmp, num)
+	}
+	for _, num := range tmp {
+		//fmt.Println(num)
+		sum += num
+	}
+
+	avg := sum / len(tmp)
+	return avg
 }
